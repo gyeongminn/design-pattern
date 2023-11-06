33d1261dd1278bbf19fcf6a5ec90b235fbec997c
Gyeongmin Lee <lee_gyeongmin@naver.com>
2023-11-06 13:41:51 +0900

    decorator example

---
+++ decorator_pattern/RoadDisplay/src/Client.java
@@ -0,0 +1,8 @@
+public class Client {
+    public static void main(String[] args) {
+        RoadDisplay road = new RoadDisplay();
+        road.draw(); // 기본 도로만 표시
+        RoadDisplay roadWithLane = new RoadDisplayWithLane();
+        roadWithLane.draw(); // 기본 도로 + 차선 표시
+    }
+}
---
+++ decorator_pattern/RoadDisplay/src/RoadDisplay.java
@@ -0,0 +1,5 @@
+public class RoadDisplay { // 기본 도로 표시 클래스
+    public void draw() {
+        System.out.println("도로 기본 표시");
+    }
+}
